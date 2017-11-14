f94edff2323db64542091ed0a6271b51f94ea254
Tom HOYO <33149950+tomhoyo@users.noreply.github.com>
2017-11-14 16:57:49 +0100

    récupérer toutes les valeurs des pouls

--- liaison_fichier_arduino/sketch_nov14a/sketch_nov14a.ino
+++ liaison_fichier_arduino/sketch_nov14a/sketch_nov14a.ino
@@ -1,14 +1,16 @@
 #include "coeur.h"
+#include "randomnombre.h"
 
 void setup()
 {
   int i;
  // i = 2;
-  
+  int SenVal;
   //Chenille(i);
   //Chenille_1sur2(i);
  //Chenille_1sur3(i);
  //Chenille_tout(i);
+   AleaNbr(SenVal);
 }
 void loop()
 {
