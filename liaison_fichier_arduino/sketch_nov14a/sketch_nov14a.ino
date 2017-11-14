d8d4b4e87915ce3b78b3bb30ffc2b8f1ee02a47b
KevinRigal <kevin_rigal@yahoo.com>
2017-11-14 17:07:24 +0100

    backup

--- liaison_fichier_arduino/sketch_nov14a/sketch_nov14a.ino
+++ liaison_fichier_arduino/sketch_nov14a/sketch_nov14a.ino
@@ -1,20 +1,13 @@
-#include "coeur.h"
-#include "randomnombre.h"
+#include "param.h"
 
 void setup()
 {
   int i;
  // i = 2;
-  int SenVal;
+  
   //Chenille(i);
   //Chenille_1sur2(i);
  //Chenille_1sur3(i);
- //Chenille_tout(i);
-<<<<<<< HEAD
-   AleaNbr(SenVal);
-=======
- //Chenille_Ay(i);
->>>>>>> 91f8e9a2ef90d59bea901c5f1c8a1224158191ea
 }
 void loop()
 {
