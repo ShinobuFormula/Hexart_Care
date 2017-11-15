dbf73b0b0f5b26f86b98cfa78db2114e3e4716d7
ShinobuFormula <damien.laporte@viacesi.fr>
2017-11-15 10:26:11 +0100

    Deus Update

--- liaison_fichier_arduino/sketch_nov14a/coeur.h
+++ liaison_fichier_arduino/sketch_nov14a/coeur.h
@@ -3,3 +3,4 @@ void Chenille_1sur2(int i);
 void Chenille_1sur3(int i);
 void Chenille_tout(int i);
 void Chenille_Ay(int i);
+
--- liaison_fichier_arduino/sketch_nov14a/sketch_nov14a.ino
+++ liaison_fichier_arduino/sketch_nov14a/sketch_nov14a.ino
@@ -1,16 +1,33 @@
 #include "coeur.h"
-
+#include "param.h"
 
 void setup()
 {
   int i;
- //i = 2;
-  //Chenille(i);
-  //Chenille_1sur2(i);
- //Chenille_1sur3(i);
-//Chenille_tout(i);
- Chenille_Ay(i);
+ // i = 2;
 
+ switch (choix)
+ { 
+  case 1 :
+    Chenille(i);
+    break;
+    
+  case 2 :
+    Chenille_1sur2(i);
+    break;
+
+  case 3 :
+    Chenille_1sur3(i);
+    break;
+
+  case 4 :
+    Chenille_tout(i);
+    break;
+
+  case 5 :
+    Chenille_Ay(i);
+    break;
+ }
 }
 void loop()
 {
