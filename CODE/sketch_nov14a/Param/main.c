52453a0314920d669cf92043e9df898d54a184cf
KevinRigal <kevin_rigal@yahoo.com>
2017-11-15 13:25:32 +0100

    update 3.2

--- CODE/sketch_nov14a/Param/main.c
+++ CODE/sketch_nov14a/Param/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "generationCode.h"
+#include "menu.h"
 int main()
 {
     int ch;
-    printf("Choississez le schema que le coeur doit reproduire \n 1.Chenille 2.Clignote 1 sur 2  3.Allume 1 sur 3  4.Tout Clignote  5.Chenille Aymeric");
+    printf("Choississez le schema que le coeur doit reproduire \n 1.Chenille 2.Clignote 1 sur 2  3.Allume 1 sur 3  4.Tout Clignote  5.Chenille ");
     scanf("%d",&ch);
     creation_fichier();
     parametres(ch);
+    return 0;
 
 }
--- CODE/sketch_nov14a/Param/menu.c
+++ CODE/sketch_nov14a/Param/menu.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "menu.h"
 
 void parametres(int ch)
 {
+    FILE *f = NULL;
 	switch(ch)
 	{
 	case 1 :
@@ -12,23 +13,24 @@ void parametres(int ch)
 	break;
 
 	case 2 :
-	
+
 		fprintf(f,"#define choix 2");
 	break;
 
 	case 3 :
-	
+
 		fprintf(f,"#define choix 3");
 	break;
 
 	case 4 :
-	
+
 		fprintf(f,"#define choix 4");
 	break;
 
 	case 5 :
-	
+
 		fprintf(f,"#define choix 5");
 	break;
 	}
+	int fclose(f);
 }
