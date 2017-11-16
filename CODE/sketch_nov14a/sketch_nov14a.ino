3e12d8702fb98edd5a384cb563fa79b3ec989849
ShinobuFormula <damien.laporte@viacesi.fr>
2017-11-16 09:00:22 +0100

    Commentaire du code Arduino P2

--- CODE/sketch_nov14a/sketch_nov14a.ino
+++ CODE/sketch_nov14a/sketch_nov14a.ino
@@ -1,14 +1,13 @@
-#include "coeur.h"
+#include "coeur.h" //Implémentation des bibliothèque créer
 #include "param.h"
 
 void setup()
 {
-  int i;
- // i = 2;
+  int i; //Déclaration de la variable i utiliser dans toute nos fonctions
 
- switch (choix)
+ switch (choix) //Vérification de la valeur de notre macro écrite dans param.h par notre programme en C
  { 
-  case 1 :
+  case 1 : //Si l'utilisateur a entré 1 lorsque qu'on téléverseras le mode chenille seras activé
     Chenille(i);
     break;
     
